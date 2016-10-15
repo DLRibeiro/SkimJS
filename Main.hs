d5d29331de2c88b9b31cb861c5676911b6a7d884
DLRibeiro <dlr4@cin.ufpe.br>
2016-10-15 14:08:33 -0300

    Adicionei meu main e value

--- Main.hs
+++ Main.hs
@@ -6,29 +6,167 @@ import Data.Map as Map (Map, insert, lookup, union, toList, empty)
 import Debug.Trace
 import Value
 
+import Data.Bits
 --
 -- Evaluate functions
 --
 
+--EXPRESSIONS
 evalExpr :: StateT -> Expression -> StateTransformer Value
 evalExpr env (VarRef (Id id)) = stateLookup env id
+evalExpr env (Numlit num) = return $ Double num
+evalExpr env (StringLit string) = return $ String string
 evalExpr env (IntLit int) = return $ Int int
 evalExpr env (BoolLit bool) = return $ Bool bool
+evalExpr env (ArrayLit []) = return $ Array []
+evalExpr env (ArrayLit l) = createArray env l (Array [])
 evalExpr env (InfixExpr op expr1 expr2) = do
     v1 <- evalExpr env expr1
     v2 <- evalExpr env expr2
     infixOp env op v1 v2
-evalExpr env (AssignExpr OpAssign (LVar var) expr) = do
-    stateLookup env var -- crashes if the variable doesn't exist
-    e <- evalExpr env expr
-    setVar var e
+evalExpr env (ListExpr []) = return Nil
+evalExpr env (ListExpr [x]) =do 
+	evalExpr env x
+evalExpr env (ListExpr (x:xs)) = do 
+	evalExpr env x >> evalExpr env xs  
 
+evalExpr env (AssignExpr OpAssign (LValue var) expr) = do
+    proc <-stateLookup env var -- crashes if the variable doesn't exist
+    e <- evalExpr env expr
+    if (proc == GlobalVar) then setGlobalVar var e else setVar var e
+evalExpr env (AssignExpr OpAssign (LBracker exp1 exp2) expr ) = do
+	case expr1 of
+		VarRef (Id id) -> do 
+			variable <- stateLookup env id
+			posDot <- evalExpr env exp2
+			e <- evalExpr env expr
+			case variable of 
+				Array l -> do
+					newArray <-createArray (Array []) (Array l) pos e
+					setVar id newArray
+				_ -> error $ "Not an array"
+evalExpr env (UnaryAssignExpr inc (LVar var)) = do
+	case inc of 
+		PrefixInc -> evalExpr env (AssignExpr OpAssign (LVar var) (InfixExpr OpAdd (VarRef (Id var)) (IntLit 1)))
+		PrefixDec -> evalExpr env (AssignExpr OpAssign (LVar var) (InfixExpr OpSub (VarRef (Id var)) (IntLit 1)))
+		PostfixInc -> evalExpr env (AssignExpr OpAssign (LVar var) (InfixExpr OpAdd (VarRef (Id var)) (IntLit 1)))
+        PostfixDec -> evalExpr env (AssignExpr OpAssign (LVar var) (InfixExpr OpSub (VarRef (Id var)) (IntLit 1)))
+
+evalExpr env (BracketRef expr1 expr2) = do 
+	evaluatedExpr1 <- evalExpr env expr1
+	evaluatedExpr2 <- evalExpr env expr2
+	findElementAt env evaluatedExpr1 evaluatedExpr2
+
+evalExpr env (CondExpr expr1 expr2 expr3) = do
+	evaluatedExpr <- evalExpr env expr1
+	case evaluatedExpr of
+		Bool True -> evalExpr env expr2
+		Bool Fasle -> evalExpr env expr3
+
+
+--ARRAY RELATED STUFFS
+createArray :: StateT ->[Expression] ->Value ->StateTransformer Value
+createArray env [] (Array l) = return $ Array l
+createArray env (x:xs) (Array l) = do
+	evaluatedExpr <- evalExpr env x
+	createArray env xs (Array (l++[evaluatedExpr])
+
+findElementAt :: StateT -> Value -> Value -> StateTransformer Value
+findElementAt env (Array []) (Int n) = return $ Nil
+findElementAt env (Array (x:xs)) (Int 0) = return x
+findElementAt env (Array (x:xs)) (Int n) = do 
+	findElementAt env (Array xs) (Int (n-1))
+
+--STATEMENTS
 evalStmt :: StateT -> Statement -> StateTransformer Value
 evalStmt env EmptyStmt = return Nil
 evalStmt env (VarDeclStmt []) = return Nil
 evalStmt env (VarDeclStmt (decl:ds)) =
     varDecl env decl >> evalStmt env (VarDeclStmt ds)
 evalStmt env (ExprStmt expr) = evalExpr env expr
+evalStmt env (IfStmt exp st1 st2) = do
+	e <- evalExpr env exp 
+    case e of 
+         Bool True -> evalStmt env st1
+         Bool False -> evalStmt env st2
+
+evalStmt env (IfSingleStmt exp st1) = do
+      e <- evalExpr env exp
+      case e of
+      	Bool True -> evalStmt env st1
+      	Bool False -> return Nil
+
+evalStmt env (ForStmt init test inc stmt) = do
+	case init of
+		NoInit -> return Nil
+		VarInit vars -> varDeclList env vars
+		ExprInit expr -> evalExpr env expr
+	case test of 
+		Nothing -> do
+			evaluatedStmt <- evalStmt env stmt
+			case evaluatedStmt of
+				Break b -> return Nil
+				Return r -> return $ (Return r)
+				_ -> do
+					case inc of 
+						Nothing -> evalStmt env (ForStmt NoInit test inc stmt)
+						Just incV -> evalExpr env incV >> evalStmt env (ForStmt NoInit test inc stmt)
+		Just bool -> do 
+			evaluatedExpr <- evalExpr env bool
+			case evaluatedExpr of
+				Bool True -> do
+					stmtEvaluated <- evalStmt env stmt
+					case stmtEvaluated of
+						Break d -> return Nil
+						Return l -> return $ (Return r)
+						_ -> do
+							case inc of
+								Nothing -> evalStmt env (ForStmt NoInit test inc stmt)
+								Just incW -> evalExpr env incV >> evalStmt env (ForStmt NoInit test inc stmt)
+				Bool False -> return Nil
+
+evalStmt env (WhileStmt exp stmt) = do
+	evaluatedExpr <- evalExpr env exp
+	case evaluatedExpr of
+		Bool True -> do 
+		e <- evalStmt env stmt 
+		case e of do
+            Break b -> return Nil
+            Return r -> return (Return r)
+			_ -> evalStmt(WhileStmt exp stmt)
+		Bool False -> return Nil
+
+evalStmt env (DoWhileStmt stmt expr) = do
+    evaluetedExpr <- evalExpr env expr
+    evaluetedStmt <- evalStmt env stmt
+    case evaluedExpr of
+        Bool True -> do
+            case evaluedStmt of
+                Break b -> return Nil
+                Return r -> return (Return r)
+                _ -> evalStmt env (DoWhileStmt stmt expr)
+        Bool False -> return Nil
+evalStmt env (BreakStmt id) = do
+	case id of
+		Nothing -> return (Break Nothing)
+		Just idM -> return (Break $ Just idM)
+evalStmt env (ReturnStmt expr) = do
+	case expr of
+		Nothing -> return (Return Nil)
+		Just exprM -> do
+			evaluatedExpr <- evalExpr env exprM
+			return (Return evaluatedExpr)
+	    
+evalStmt env (BlockStmt []) = Nil
+evalStmt env (BlockStmt (x:xs)) = do
+	--pushScope
+	evalBlock env (BlockStmt (x:xs))
+	--popScope
+
+evalBlock :: StateT -> Statement -> StateTransformer Value
+evalBlock env (BlockStmt []) = Nil
+evalBlock env (BlockStmt (x:xs)) = do
+	evalStmt env x >> evalBlock env (BlockStmt xs)
 
 -- Do not touch this one :)
 evaluate :: StateT -> [Statement] -> StateTransformer Value
@@ -54,37 +192,83 @@ infixOp env OpEq   (Bool v1) (Bool v2) = return $ Bool $ v1 == v2
 infixOp env OpNEq  (Bool v1) (Bool v2) = return $ Bool $ v1 /= v2
 infixOp env OpLAnd (Bool v1) (Bool v2) = return $ Bool $ v1 && v2
 infixOp env OpLOr  (Bool v1) (Bool v2) = return $ Bool $ v1 || v2
+infixOp env OpXor  (Bool v1) (Bool v2) = return $ Bool $ v1^v2
+infixOp env OpBor (Bool v1) (Bool v2 ) = return $ Bool $ v1 | v2
+infixOp env OpLShift (Int v1) (Int v2 ) = return $ Int $ shiftl v1 v2
+infixOp env OpSpRShift  (Int v1) (Int v2) = return $ Int $ shiftR v1 v2
+
+prefixOp :: StateT -> PrefixOp -> Value -> StateTransformer Value
+
+prefixOp env PrefrixLNot (Bool v1) = return $ Bool $ not v1
+prefixOp env PrefrixBNot (Bool v1) = return $ Bool $ not v1
+prefixOp env PrefixMinus (Int v1) = return $ Int $ -v1
+prefixOp env PrefixMinus (Double v1) = return $ Double $ -v1
+
 
 --
 -- Environment and auxiliary functions
 --
 
-environment :: Map String Value
-environment = Map.empty
+environment :: StateT
+environment = [Map.empty]
+
+
 
 stateLookup :: StateT -> String -> StateTransformer Value
 stateLookup env var = ST $ \s ->
     -- this way the error won't be skipped by lazy evaluation
-    case Map.lookup var (union s env) of
-        Nothing -> error $ "Variable " ++ show var ++ " not defiend."
+    case ScopeLookup s var of
+        Nothing -> (GlobalVar,s)
         Just val -> (val, s)
 
+varDeclList :: StateT -> [VarDecl] -> StateTransformer Value
+varDeclList env [] = return Nil
+varDeclList env var = foldl1 (>>) map (varDecl env) var 
+		
 varDecl :: StateT -> VarDecl -> StateTransformer Value
 varDecl env (VarDecl (Id id) maybeExpr) = do
     case maybeExpr of
-        Nothing -> setVar id Nil
+        Nothing -> setLocalVar id Nil
         (Just expr) -> do
             val <- evalExpr env expr
-            setVar id val
+            setLocalVar id val
 
 setVar :: String -> Value -> StateTransformer Value
-setVar var val = ST $ \s -> (val, insert var val s)
+setVar var val = ST $ \s -> (val, setVarScopes var val s)
 
+setLocalVar ::String -> Value -> StateTransformer Value
+setLocalVar var val = ST $ \s -> (val, (insert var val (head s)):(tail s))
+
+setGlobalVar :: String ->Value ->StateTransformer Value
+setGlobalVar var val = ST $ \s -> (val, setGlobalAux var val s)
+
+setGlobalAux :: String -> Value ->StateT ->StateTransformer
+setGlobalAux var val (s:scopes) = if(scopes == [])
+                                   then (insert var val s):scopes
+									else s:(setGlobalAux var val scopes)
+
+setVarScopes :: String -> Value -> StateT -> StateT
+setVarScopes var val _ = error $ "Variavel nao existe"
+setVarScopes var val st = case (Map.lookup var (head st) of
+                          Nothing -> (head st):(setVarScopes var val (tail st))
+						  Just v -> (insert var val (head st)):(tail st)
+
+--ESCOPO- VARIAVEIS LOCAIS E GLOBAIS
+scopeLookup :: StateT -> String -> Maybe Value
+scopeLookup [] var = Nothing
+scopeLookup (scope:scopes) var = case Map.lookup var s of 
+                                 Nothing -> scopeLookup scopes var 
+                                 Just val-> Just val
+pushScope:: StateT
+pushScope = ST $ \s -> (Nil, (Map.empty):s)
+
+popScope:: StateT
+popScope = ST $	\s -> (Nil,(tail s))							 
 --
 -- Types and boilerplate
 --
-
-type StateT = Map String Value
+type maping = Map String Value
+type StateT = [maping]
 data StateTransformer t = ST (StateT -> (t, StateT))
 
 instance Monad StateTransformer where
@@ -110,7 +294,7 @@ showResult (val, defs) =
     show val ++ "\n" ++ show (toList $ union defs environment) ++ "\n"
 
 getResult :: StateTransformer Value -> (Value, StateT)
-getResult (ST f) = f Map.empty
+getResult (ST f) = f [Map.empty]
 
 main :: IO ()
 main = do
--- Value.hs
+++ Value.hs
@@ -1,10 +1,20 @@
 module Value (Value (..)) where
+import Language.ECMAScript3.Syntax
 
 data Value = Bool Bool
     | Int Int
     | String String
     | Var String
     | Nil
+	  | GlobalVar
+	  | Double Double
+	  | Break (Maybe Id)
+	  | Function Id [Id] [Statement]
+	  | Return Value
+	  | Array [Value]
+	  | Empty
+	deriving (Eq)
+	
 
 --
 -- Pretty Printer
@@ -17,10 +27,23 @@ instance Show Value where
   show (String str) = "\"" ++ str ++ "\""
   show (Var name) = name
   show Nil = "undefined"
+  show (Function id [args] [comands] ) = "Func: " ++ id ++ (showArgs args)
+  show (Return v) = show v
+  show (Array b) = "[" ++ (showArray (Array b)) ++ "]"
+  show GlobalVar = "Undefined variable"
+  show (Double d) = show d
+  
   
 -- This function could be replaced by (unwords.map show). The unwords
 -- function takes a list of String values and uses them to build a 
 -- single String where the words are separated by spaces.
+showArgs [] = ""
+showArgs (Id c :cs) = (show c) ++ ";" ++ showArgs cs
+
+showArray [] = "";
+showArray [b] = show b
+showArray (Array (b:bs)) = show b ++ "," showArray bs
+
 showListContents :: [Value] -> String
 showListContents [] = ""
 showListContents [a] = show a
